b8458ba20aebbeca71ca0e152a5b16ef52786a9b
Stephen Coloma <2232847@slu.edu.ph>
2024-11-07 23:39:13 +0800

    add added dotend and env files for more security

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello, world');
+import express, { Request, Response } from 'express';
+import env from 'dotenv'
+
+env.config();
+
+const app = express();
+const HOST = process.env.HOST as string;
+const PORT = parseInt(process.env.PORT as string);
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, world')
+})
+
+app.listen(PORT, HOST, () => {
+    console.log(`Example app listening on port ${PORT}`)
+})
+
