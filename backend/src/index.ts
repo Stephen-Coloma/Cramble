8b6b407aeda61406db3dfbee591b68095d3e2f1a
Stephen Coloma <2232847@slu.edu.ph>
2024-11-16 20:59:03 +0800

    add: add additional modules such as cookie-parser and helmet modified also env files to have secret keys stored properly

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,33 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import config from './config/config'
-import loginRouter from './routes/login'
-import signupRouter from './routes/signup'
-import homepageRouter from './routes/homepage'
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import config from './config/config';
+import loginRouter from './routes/login';
+import signupRouter from './routes/signup';
+import homepageRouter from './routes/homepage';
+import env from 'dotenv';
 
 // config
+env.config();
+
 const hostname = config.server.hostname;
 const port = config.server.port;
 const app = express();
 app.use(bodyParser.json())
+app.use(cookieParser(process.env.COOKIE_PARSER_SECRET_KEY || undefined))
+app.use(helmet());
 
 // routers
 //prefix: /api/
 app.use('/api/', homepageRouter)
 app.use('/api/', signupRouter)
 app.use('/api/', loginRouter)  
+
+//custom 404
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that!")
+})
 
 app.listen(port, hostname, () => {
     console.log(`Server listening on port ${port}`);
