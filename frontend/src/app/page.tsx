c2faa391dd9ff5e091393cf0ea3bd9c8dc7a7ab5
Stephen Coloma <2232847@slu.edu.ph>
2025-01-10 21:48:14 +0800

    add: added the background theme of the web app changed the logo of the facebook in logging in and siging up

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,8 +2,6 @@ import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
-    <div>Hello world
-      <ModeToggle></ModeToggle>
-    </div>
+    <div className="bg-[url('/assets/cramble-background.svg')] bg-cover bg-center h-screen w-full"><ModeToggle></ModeToggle></div>
   );
 }
