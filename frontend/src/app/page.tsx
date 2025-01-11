20409d59ef797e0365f45b12ea19c98df47ff154
Stephen Coloma <2232847@slu.edu.ph>
2025-01-11 20:27:00 +0800

    add: added the header of the application

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,10 @@
 import { ModeToggle } from "@/components/mode-toggle";
+import Header from "@/components/header";
 
 export default function Home() {
   return (
-    <div className="bg-[url('/assets/cramble-background.svg')] bg-cover bg-center h-screen w-full"><ModeToggle></ModeToggle></div>
+    <div className="bg-[url('/assets/cramble-background.svg')] bg-cover bg-center h-screen w-full">
+      <Header></Header>
+    </div>
   );
 }
---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import { ModeToggle } from "./mode-toggle"
+
+export default function Header() {
+  return (
+    <div className="container mx-auto px-4 md:px-6 lg:px-8">
+      <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
+        <Link href="#" className="mr-6 lg:flex items-center" prefetch={false}>
+          <img src="/assets/Black-Cramble-Logo.svg" className="h-8 w-8" />
+        </Link>
+        <div className="ml-auto flex gap-2 px-4 py-2 rounded-md bg-secondary border">
+          <Button variant="default" className="justify-self-end">
+            <a href="/login" className="text-white">Login</a>
+          </Button>
+          <ModeToggle></ModeToggle>
+        </div>
+      </header>
+    </div>
+  )
+}
