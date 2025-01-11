9167d35c42f5331ee40302ba226e942e3acaf79a
Stephen Coloma <2232847@slu.edu.ph>
2025-01-11 22:58:38 +0800

    add: added the header and footer of the page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
-import { ModeToggle } from "@/components/mode-toggle";
-import Header from "@/components/header";
+import {Header} from "@/components/header";
+import { Footer } from "@/components/footer";
 
 export default function Home() {
   return (
-    <div className="bg-[url('/assets/cramble-background.svg')] bg-cover bg-center h-screen w-full">
+    <div className="flex flex-col h-screen w-full bg-[url('/assets/cramble-background.svg')] bg-cover bg-center">
       <Header></Header>
+      <div className="flex-grow">CONTENT FOT THE WHOLE PAGE</div> 
+      <Footer></Footer>
     </div>
   );
 }
--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { ModeToggle } from "./mode-toggle"
 
-export default function Header() {
+export function Header() {
   return (
     <div className="container mx-auto px-4 md:px-6 lg:px-8">
       <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
