a18c7337ad1e73fab0b6b315c69b73309be8fffb
Stephen Coloma <2232847@slu.edu.ph>
2025-01-13 22:45:50 +0800

    add: added the sidebar shadcn component edited the component to suit my application navigations edited the sidebar header and removed components that are'nt used

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,10 +7,16 @@ export default function Home() {
   return (
     <div className="flex flex-col h-screen w-full bg-[url('/assets/cramble-background.svg')] bg-cover bg-center"> 
       <Header></Header>
-      <div className="container mx-auto flex flex-col flex-grow items-center gap-10 pt-16 xl:px-96">
-          <Button size={"sm"} className="text-white hover:none cursor-default rounded-full">Online Flashcards Reviewer</Button>
-          <h1 className="text-center text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl/none bg-gradient-to-r from-muted-foreground via-foreground to-muted-foreground bg-clip-text text-transparent">Redefining how you learn, practice and grow.</h1>
-          <p className="text-center text-sm text-muted-foreground sm:text-base md:text-lg">Master your quizzes with personalized <br></br>flashcards — organized, trackable and <br></br>seamless.</p>
+      <div className="container mx-auto flex flex-col flex-grow items-center gap-10 pt-16 xl:px-96 md:pt-10">
+          <Button size={"sm"} className="text-white hover:none cursor-default rounded-full">
+            Online Flashcards Reviewer
+          </Button>
+          <h1 className="text-center text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl/none bg-gradient-to-r from-muted-foreground via-foreground to-muted-foreground bg-clip-text text-transparent">
+            Redefining how you learn, practice and grow.
+          </h1>
+          <p className="text-center text-sm text-muted-foreground sm:text-base md:text-lg">
+              Master your quizzes with personalized <br></br>flashcards — organized, trackable and <br></br>seamless.
+          </p>
           <Button variant={"default"} size={"lg"} className="w-fit">
             <a href="/signup" className="text-white">Get Started</a>
             <ArrowUpRight color="white"></ArrowUpRight>
