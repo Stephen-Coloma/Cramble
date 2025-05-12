63a3b242af0771659f73252125078d23b5ea95dd
Stephen Coloma <2232847@slu.edu.ph>
2025-05-12 13:44:06 +0800

    ref(date util fn) refactoring date format code
    This commmit creates a fucntion in the utils lib that formats the date.

---
+++ frontend/src/lib/date.ts
@@ -0,0 +1,10 @@
+export function formatDate(date: string): string{
+    return new Date(date).toLocaleDateString(
+        "en-US",
+        {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        }
+    )
+}
