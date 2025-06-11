242e26f6a6b68705eb89ddb58ff5b9e145f2dac4
Stephen Coloma <2232847@slu.edu.ph>
2025-06-11 15:36:51 +0800

    fix(moved page) explore, faqs
    This commit removes the two page called explore and faqs page.
    It is because these two pagest apparently are not as important as it seems anymore

--- frontend/src/app/dashboard/faqs/page.tsx
+++
@@ -1,9 +0,0 @@
-import { PageHeader } from "@/components/page-header";
-
-export default function FAQs() {
-  return (
-    <div className=" w-full border-2 border-blue-900">
-      <PageHeader route="/dashboard/faqs"/>
-    </div>
-  )
-} 
